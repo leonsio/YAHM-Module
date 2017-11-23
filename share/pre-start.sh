eecc6244c2d1dedabfc8796e6ce180738ab4f8ce
Leonid Kogan <leon@leonsio.com>
2017-11-23 21:00:15 +0100

    Erster Release mit PIVCCU Support, Danke an Alex Reinert https://github.com/alexreinert/piVCCU Alte Module werden nach und nach entfernt, bzw. abgelöst

---
+++ share/pre-start.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#LXC_CONFIG_FILE=/var/lib/lxc/yahm/config
+#LXC_ROOTFS_PATH=/var/lib/lxc/yahm/root
+#LXC_NAME=yahm
+
+if [ -f /var/lib/lxc/${LXC_NAME}/.modules/pivccu ] 
+then
+	YAHM_KERNEL="pivccu"
+	YAHM_DEVICE="raw-uart"
+elif [ -f /var/lib/lxc/${LXC_NAME}/.modules/homematic-ip ] 
+then
+	YAHM_KERNEL="homematic-ip"
+	YAHM_DEVICE="bcm2835-raw-uart"
+else
+	YAHM_KERNEL="none"
+	exit 0
+fi
+
+if [ -d /sys/devices/virtual/eq3loop ] 
+then
+	EQ3LOOP_MAJOR=`cat /sys/devices/virtual/eq3loop/eq3loop/dev | cut -d: -f1`
+	echo -n $EQ3LOOP_MAJOR > /sys/module/plat_eq3ccu2/parameters/eq3charloop_major
+fi
+if [ "${YAHM_KERNEL}" != "none" ] 
+then
+	UART_MAJOR=`cat /sys/devices/virtual/${YAHM_DEVICE}/${YAHM_DEVICE}/dev | cut -d: -f1`
+	echo -n $UART_MAJOR > /sys/module/plat_eq3ccu2/parameters/uart_major
+fi
+#todo ggf checks einbauen ob alles richtig installiert wurde?
---
+++ share/yahm-start.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+EQ3LOOP_MAJOR=`cat /sys/module/plat_eq3ccu2/parameters/eq3charloop_major`
+UART_MAJOR=`cat /sys/module/plat_eq3ccu2/parameters/uart_major`
+
+version () { echo "$@" | awk -F. '{ printf("%03d%03d%03d\n", $1,$2,$3); }'; }
+
+# generate/update dev nodes
+
+rm -f /dev/eq3loop
+rm -f /dev/ttyS0
+rm -f /dev/mmd_bidcos
+rm -f /dev/mxs_auart_raw.0
+
+mknod -m 666 /dev/mxs_auart_raw.0 c $UART_MAJOR 0
+
+# get radio mac and serial
+/bin/eq3configcmd update-coprocessor -p /dev/mxs_auart_raw.0 -t HM-MOD-UART -c -se 2>&1 | grep "SerialNumber:" | cut -d' ' -f5 > /sys/module/plat_eq3ccu2/parameters/board_serial
+/bin/eq3configcmd read-default-rf-address -f /dev/mxs_auart_raw.0 -h | grep "^0x" > /sys/module/plat_eq3ccu2/parameters/radio_mac
+
+firmware_version=`/bin/eq3configcmd update-coprocessor -p /dev/mxs_auart_raw.0 -t HM-MOD-UART -c -v 2>&1 | grep "Version:" | cut -d' ' -f5`
+echo $firmware_version > /sys/module/plat_eq3ccu2/parameters/board_firmware
+
+if [ "$(version "$firmware_version")" -lt "$(version "2.0.0")" ]; then
+	ln -sf /dev/mxs_auart_raw.0 /dev/mmd_bidcos
+else
+	mknod -m 666 /dev/eq3loop c $EQ3LOOP_MAJOR 0                               
+	mknod -m 666 /dev/ttyS0 c $EQ3LOOP_MAJOR 1  
+	mknod -m 666 /dev/mmd_bidcos c $EQ3LOOP_MAJOR 2
+fi
