e1ca07aa6a8071a883ed5721482c082fa8e75d3d
Leonid Kogan <leon@leonsio.com>
2018-01-11 11:54:07 +0100

    Anpassung board_firmware auf board_extended_info

--- share/yahm-start.sh
+++ share/yahm-start.sh
@@ -19,7 +19,7 @@ mknod -m 666 /dev/mxs_auart_raw.0 c $UART_MAJOR 0
 /bin/eq3configcmd read-default-rf-address -f /dev/mxs_auart_raw.0 -h | grep "^0x" > /sys/module/plat_eq3ccu2/parameters/radio_mac
 
 firmware_version=`/bin/eq3configcmd update-coprocessor -p /dev/mxs_auart_raw.0 -t HM-MOD-UART -c -v 2>&1 | grep "Version:" | cut -d' ' -f5`
-echo $firmware_version > /sys/module/plat_eq3ccu2/parameters/board_firmware
+echo $firmware_version > /sys/module/plat_eq3ccu2/parameters/board_extended_info
 
 if [ "$(version "$firmware_version")" -lt "$(version "2.0.0")" ]; then
 	ln -sf /dev/mxs_auart_raw.0 /dev/mmd_bidcos
